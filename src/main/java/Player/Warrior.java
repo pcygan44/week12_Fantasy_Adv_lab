3ee4ae5632fc4e2306aae0b75202d5b42bef29aa
Andrew Thomas <ajthomas145@gmail.com>
2021-11-24 14:15:49 +0000

    added player abstract class

---
+++ src/main/java/Player/Warrior.java
@@ -0,0 +1,12 @@
+package Player;
+
+import behaviours.IWeapon;
+
+public abstract class Warrior extends Player implements IWeapon {
+
+    public Warrior(String name) {
+        super(name);
+    }
+
+    
+}
